ff20f549b7a5a2fda32913f5fb764acbba752fca
Mofe <moflux620@gmail.com>
2022-08-08 17:10:36 +0100

    looking good

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,17 +1,95 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import he from "he";
 
-export default function Question({data}) {
-  const options = data.incorrect_answers;
-  options.push(data.correct_answer);
+export default function Question({raw, success, setSuccess}) {
+
+  const json = JSON.stringify(raw);
+  const format1 = json.replace(/\\n/g, "\\n")  
+  .replace(/\\'/g, "\\'")
+  .replace(/\\"/g, '\\"')
+  .replace(/\\&/g, "\\&")
+  .replace(/\\r/g, "\\r")
+  .replace(/\\t/g, "\\t")
+  .replace(/\\b/g, "\\b")
+  .replace(/\\f/g, "\\f")
+  .replace(/&quot;/g,"\\\""); 
+  const format2 = he.decode(format1); //convert html special entities like &euml; or &apos; to the proper character
+  const data = JSON.parse(format2);
+  const incorrect = data.incorrect_answers;
+  const correct = data.correct_answer;
+  const options = [];
+  incorrect.forEach(value => options.push({
+    content: value,
+    status: false,
+    picked: false
+  }));
+  options.push({
+    content: correct,
+    status: true,
+    picked: false,
+    id: uuidv4()
+  });
+  //status indicates if an option is the correct answer or not
+  //picked indicates if an option has been selected by the user
+
+  //SHUFFLE OPTIONS
+  const shuffledOptions = options.sort(function () {
+    return Math.random() - 0.5;
+  });
+  
+  //SELECT OPTION
+  function selectOption(e, value, id){
+    for (let i = 0; i < shuffledOptions.length; i++) {
+      if (shuffledOptions[i].content === value){
+        shuffledOptions[i].picked = true;
+        const choice = e.target;
+        choice.classList.add('selected', 'picked');//selected is referring to the UI changes that ocuur when an option is clicked. Picked as a class helps to display the red color for those picked answers that are wrong when user submits
+        const parent = choice.parentElement; //selects parent <ul></ul>
+        const childTags = parent.children; //selects all <li></li> tags within the parent
+        for (let v = 0; v < childTags.length; v++) {
+          if(childTags[v] !== choice){
+            childTags[v].classList.remove('selected');
+          }
+        }
+        if(shuffledOptions[i].status === true){
+          if(!success.includes(value)){
+            success.push(value);
+            setSuccess(success);
+            console.log(success);
+          }
+        } else {
+          //if first attempt is wrong, do nothing
+          //if user changes right answer to wrong answer, remove right answer and update success array
+          const optionsArray = shuffledOptions.map(option => option.content);
+          // eslint-disable-next-line
+          const common = optionsArray.filter(element => success.includes(element));
+          console.log(common);
+          if(common.some(r=> optionsArray.includes(r))){
+            const modify = success.filter(item => !common.includes(item));
+            setSuccess(modify);
+            console.log(success);
+          }
+        }
+      } else {
+        shuffledOptions[i].picked = false;
+      }
+    }
+  }
+
   return (
     <>
-        <div className='question'>
-          <p>{data.question}</p>
-          <ul>
-           {options.map(option => <li key={uuidv4()}>{option}</li>)}
-          </ul>
-        </div>
+      <div className='question'>
+        <p>{data.question}</p>
+        <ul>
+          {shuffledOptions.map(option => <li onClick={(e)=>{selectOption(e, option.content, option.id)}} className={option.status ? "correct" : "incorrect"} key={uuidv4()}>{option.content}{option.selected}</li>)}
+        </ul>
+      </div>
     </>
   )
 }
+
+//WHEN RESULTS ARE DISPLAYED
+//Options with class of correct i.e status of true will be green
+//Options with class of incorrect and picked i.e status of false, picked of true will be red
+//Options with class of incorrect i.e status of false but picked of false will remain the same
--- src/routes/Quiz.js
+++ src/routes/Quiz.js
@@ -1,28 +1,48 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import Question from '../components/Question';
 import { v4 as uuidv4 } from 'uuid';
 
 export default function Quiz() {
-  const [questions, setQuestions] = React.useState(null);
+  const [questions, setQuestions] = useState(null);
+  const [success, setSuccess] = useState([]);
   
-  const getQuestions = React.useCallback(async () => {
+  const getQuestions = useCallback(async () => {
     const request = await fetch('https://opentdb.com/api.php?amount=5&type=multiple');
     const response = await request.json();
     const questionSet = response.results;
     setQuestions(questionSet);
     return questionSet;
   },[])
+
   useEffect(() => {
    getQuestions()
   },[getQuestions])
 
+  function showAnswers(){
+    const questionField = document.querySelector('.quiz');
+    questionField.setAttribute('class', 'quiz mark');
+    document.querySelector('.play_again').style.display = 'flex';
+    document.querySelector('#submit').style.display = "none";
+
+    //Count number of Correct Picks
+
+  }
+
+
+
   return (
     <div className='quiz'>
-      <h3>Answer 5 Questions</h3>
+      {questions && <h3>Answer 5 Questions</h3>}
       <div className="">
-        {questions != null ? questions.map(question =>  <Question key={uuidv4()} data={question} />) : "Loading..."}
+        {questions != null ? questions.map(question =>  <Question key={uuidv4()} success={success} setSuccess={setSuccess} raw={question} />) : "Loading..."}
       </div>
-     {questions && <button>Check answers</button>}
+
+      {questions && <button id='submit' onClick={showAnswers}>Check answers</button>}
+
+      <div className="play_again">
+        {questions && <p>You scored {success.length}/{questions.length} correct answers</p>}
+        <button onClick={() => window.location.reload()}>Play again</button>
+      </div>
     </div>
   )
 }
